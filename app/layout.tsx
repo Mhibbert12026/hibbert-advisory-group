daf2a6cb7886360fce7d0ae2552cd88ac56020f2
Mike Daevon Hibbert <mikedaevonhibbert@MacBookAir.attlocal.net>
2026-06-01 15:18:03 -0400

    Fix favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
